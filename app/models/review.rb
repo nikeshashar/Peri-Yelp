3b4888f358c13b9e3f71d673d308f65d5b76e7a7
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-13 11:12:25 +0100

    Added validation for review only being valid if there is a restaurant

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
 	VALID_RATINGS = (1..5)
 	belongs_to :restaurant
 	validates :rating, inclusion: VALID_RATINGS
+	validates :restaurant_id, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe Review, :type => :model do
 			review = Review.new(rating: -5)
 			expect(review).to have(1).error_on(:rating)
 		end
+
+		it 'is invalid if not associated with a restaurant' do 
+			review = Review.new(rating: 5)
+			expect(review).to have(1).error_on(:restaurant_id)
+		end
 	end
 
 end
