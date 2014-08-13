4075115a42289dbe19c437b61f28f5784f84d703
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-13 11:18:36 +0100

    Added test to reviews to make sure everything works

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe Review, :type => :model do
   	
   	context 'review score validation' do 
+
+  		it 'is valid with a rating between 1 and 5 and associated with a restaurant' do 
+  			restaurant = Restaurant.create(name: 'Nandos', location_name: 'Shoreditch')
+  			review = restaurant.reviews.new(rating: 5)
+			expect(review).to be_valid
+  		end
+
 		it 'is invalid if the rating is > 5' do 
 			review = Review.new(rating: 10)
 			expect(review).to have(1).error_on(:rating)
