a0be94b59be0506cc99c2b490a3341684edc5dae
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-16 20:05:38 +0100

    Testing user login

--- spec/features/restaurant_features_spec.rb
+++ spec/features/restaurant_features_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
-	context 'restaurants' do 
+	context 'logged out' do 
+		it 'should take us to a sign up page' do 
+			visit '/restaurants'
+			click_link 'Create a restaurant'
+			expect(page).to have_content 'Sign up'
+		end
 
+		it 'shows no edit link' do
+			visit '/restaurants'
+			expect(page).not_to have_content 'Edit Nandos'
+		end
+
+
+		it 'shows no delete link' do
+			visit '/restaurants'
+			expect(page).not_to have_content 'Delete Nandos'
+		end
+	end
+
+	context 'restaurants' do 
 		it 'should have no restaurants' do 
 			visit '/restaurants'
 			expect(page).to have_content 'No restaurants yet'
@@ -36,44 +54,45 @@ require 'rails_helper'
 	end
 
 
-	context 'creating a restaurant' do
-		it 'should fill in a form with the details and end up on the index page' do
-			visit '/restaurants'
-			click_link 'Add a restaurant'
-			fill_in 'Name', with: "Nandos"
-			fill_in 'Location name', with: 'shoreditch'
-			click_button 'Create Restaurant'
-			expect(page).to have_content 'Nandos'
-			expect(current_path).to eq '/restaurants'
+	context 'logged in' do 
+		context 'creating a restaurant' do
+			it 'should fill in a form with the details and end up on the index page' do
+				visit '/restaurants'
+				click_link 'Add a restaurant'
+				fill_in 'Name', with: "Nandos"
+				fill_in 'Location name', with: 'shoreditch'
+				click_button 'Create Restaurant'
+				expect(page).to have_content 'Nandos'
+				expect(current_path).to eq '/restaurants'
+			end
+		end
+
+		context 'editing restaurants' do
+			before do 
+				Restaurant.create(name: 'Nandos', phone_number: '02089540598', location_name: 'shoreditch')
+			end
+
+			it 'should allow user to edit restaurant' do
+				visit '/restaurants'
+				click_link 'Edit Nandos'
+				fill_in 'Name', with: 'Nandos - Edgware'
+				click_button 'Update Restaurant'
+				expect(page).to have_content 'Nandos - Edgware'
+				expect(current_path).to eq '/restaurants'
+			end
+		end
+
+		context 'deleting restaurants' do
+			before do 
+				Restaurant.create(name: 'Nandos', phone_number: '02089540598', location_name: 'shoreditch')
+			end
+
+			it 'should remove restaurant when user clicks delete link' do
+				visit '/restaurants'
+				click_link 'Delete Nandos'
+				expect(page).not_to have_content 'shoreditch'
+				expect(page).to have_content 'Restaurant deleted successfully'
+			end
 		end
 	end
-
-	context 'editing restaurants' do
-		before do 
-			Restaurant.create(name: 'Nandos', phone_number: '02089540598', location_name: 'shoreditch')
-		end
-
-		it 'should allow user to edit restaurant' do
-			visit '/restaurants'
-			click_link 'Edit Nandos'
-			fill_in 'Name', with: 'Nandos - Edgware'
-			click_button 'Update Restaurant'
-			expect(page).to have_content 'Nandos - Edgware'
-			expect(current_path).to eq '/restaurants'
-		end
-	end
-
-	context 'deleting restaurants' do
-		before do 
-			Restaurant.create(name: 'Nandos', phone_number: '02089540598', location_name: 'shoreditch')
-		end
-
-		it 'should remove restaurant when user clicks delete link' do
-			visit '/restaurants'
-			click_link 'Delete Nandos'
-			expect(page).not_to have_content 'shoreditch'
-			expect(page).to have_content 'Restaurant deleted successfully'
-		end
-	end
-
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
-
-	context 'reviewing restaurants' do 
-		before do
-			Restaurant.create(name: 'Nandos', location_name: 'Shoreditch')
+	
+	context 'logged out' do 
+		it 'does not show the review form' do
+			visit '/restaurants'
+			expect(page).not_to have_field('comments')
 		end
+	end
 
-		it 'allows users to leave a review using a form which appear alongside restaurants' do 			
-			leave_review('I liked it', 3)
-			expect(current_path).to eq restaurants_path
-			expect(page).to have_content 'I liked it (★★★)'
-		end
+	context 'logged in' do 
+		context 'reviewing restaurants' do 
+			before do
+				Restaurant.create(name: 'Nandos', location_name: 'Shoreditch')
+			end
 
-		it 'will display the average rating from all reviews' do 
-			leave_review('I liked it', 3)
-			leave_review('Great', 5)
-			expect(page).to have_content 'Average Rating: ★★★★' 
+			it 'allows users to leave a review using a form which appear alongside restaurants' do 			
+				leave_review('I liked it', 3)
+				expect(current_path).to eq restaurants_path
+				expect(page).to have_content 'I liked it (★★★)'
+			end
+
+			it 'will display the average rating from all reviews' do 
+				leave_review('I liked it', 3)
+				leave_review('Great', 5)
+				expect(page).to have_content 'Average Rating: ★★★★' 
+			end
 		end
 	end
