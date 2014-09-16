cbb98752d558ea08be37066a6774025cd39fac6c
nikesh <nikeshashar@gmail.com>
2014-09-16 22:39:49 +0100

    Fixed JS errors

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	$('.like-link').on('click', function(event) {
 		
-		var likeCount = $(this).siblings('.likes_count')
+		var likeCount = $(this).siblings('.likes_count');
 		event.preventDefault();
 		
 		$.post(this.href, function(response){
 			likeCount.text(response.new_likes_count);
 		});
-	})
-})
+	});
+});
 
