128c1886f8fb0f9652874756e33fd7f3e8a9b2be
nikesh <nikeshashar@gmail.com>
2014-10-02 23:21:30 +0100

    Updating layout for average rating

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Review, :type => :model do
   	
   	context 'review score validation' do 
 
-  		it 'is valid with a rating between 1 and 5 and associated with a restaurant' do 
-  			restaurant = Restaurant.create(name: 'Nandos', location_name: 'Shoreditch')
-  			review = restaurant.reviews.new(rating: 5)
-			expect(review).to be_valid
-  		end
+		it 'is valid with a rating between 1 and 5 and associated with a restaurant' do 
+			restaurant = Restaurant.create(name: 'Nandos', location_name: 'Shoreditch')
+			review = restaurant.reviews.new(rating: 5)
+		  expect(review).to be_valid
+		end
 
 		it 'is invalid if the rating is > 5' do 
 			review = Review.new(rating: 10)
